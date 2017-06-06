9219ad0112b86f78c904b7be17238bbdceed14a8
dpatel98 <dpatel0698@gmail.com>
2017-06-06 17:26:06 -0400

    One-time pad implemented

---
+++ one_time_pad.py
@@ -0,0 +1,24 @@
+from many_time_pad import strxor, toascii, tohex
+import random
+
+def key_gen(length):
+    return ''.join(str((random.randint(0, 1) + random.randint(0, 1)) % 2) for x in range(length))
+
+
+def encrypt(message, key=None):
+    if key is None:
+        gen = key_gen(len(message)*16)
+        return strxor(message, tohex(gen))
+
+    return strxor(message, toascii(key))
+
+def decrypt(cipher, key):
+    return strxor(cipher, key)
+
+if __name__ == '__main__':
+    message = 'I am the best'
+    real_key = key_gen(len(message)*16)
+    encryption = encrypt(message, real_key)
+    print('Encrypted cipher: ' + encryption)
+    print('Decrypted cipher: ' + decrypt(encryption, toascii(real_key)))
+
